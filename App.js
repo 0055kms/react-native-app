e11351986af256f08f1548b4c5d6bc6af8dd1724
kms <dooly6276@gmail.com>
2024-07-17 12:20:31 +0900

    Title, Input

--- App.js
+++ App.js
@@ -1,21 +1,2 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {theme} from './scr/theme';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>a</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import App from './src/App';
+export default App;
